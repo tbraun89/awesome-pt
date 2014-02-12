7f86ccd8d8e9ad4e5177fb0d78e3522343ea1cd8
tbraun <tbraun@tnt-web-solutions.de>
2014-02-12 20:36:39 +0100

    initial commit with awesome-pt and install + help function

---
+++ lib/install.lua
@@ -0,0 +1,14 @@
+local install = {}
+
+function install.main(arg)
+	if #arg > 1 then
+		print("TODO // implement installtion")
+		for i = 2, #arg do
+			print(arg[i])
+		end
+	else
+		error()
+	end
+end
+
+return install
